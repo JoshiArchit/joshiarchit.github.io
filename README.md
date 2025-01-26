d28b4339e8f13c1159b860b7c0e7f8f2b8d3e3e0
architJoshi2904 <aj6082@rit.edu>
2025-01-26 12:24:24 -0600

    Main graphic on landing page. Style accomodations for responsive design

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Model references - SketchFab
+# Portfolio
+
+### Some resources 
+1. 3D assets and textures (in gltf format) - https://sketchfab.com/
+2. Converting gltf to React fiber components - https://gltf.pmnd.rs/
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,25 +1,74 @@
-import { PerspectiveCamera } from '@react-three/drei'
-import React from 'react'
-import HackerRoom from '../components/HackerRoom'
+import { PerspectiveCamera } from "@react-three/drei";
+import React, { Suspense } from "react";
+import HackerRoom from "../components/HackerRoom";
+import CanvasLoader from "../components/Loading";
+import { Canvas } from "@react-three/fiber";
+import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
 
 export const Hero = () => {
+  // Leva controls for ThreeJS components - Uncomment to play with the controls. Add the controls to the respective components.
+  // const controls = useControls("HackerRoom", {
+  //   rotationX: {
+  //     value: 0,
+  //     min: -Math.PI,
+  //     max: Math.PI,
+  //   },
+  //   rotationY: {
+  //     value: -Math.PI,
+  //     min: -Math.PI,
+  //     max: Math.PI,
+  //   },
+  //   rotationZ: {
+  //     value: 0,
+  //     min: -Math.PI,
+  //     max: Math.PI,
+  //   },
+  //   scale: {
+  //     value: 0.1,
+  //     min: 0.1,
+  //     max: 1,
+  //   },
+  // });
+
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth:768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
   return (
-    <section className="min-h-screen w-full flex flex-col relative">
-        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-            <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I'm Archit <span className="waving-hand">👋</span></p>
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, I'm Archit <span className="waving-hand">👋</span>
+        </p>
 
-            <p className="hero_tag text-gray_gradient space-x-0">Building software and solutions that bridge the gap between technology and people, driving accessibility, adoption, and meaningful impact.</p>
-        </div>
+        <p className="hero_tag text-gray_gradient space-x-0">
+          Building software and solutions that bridge the gap between technology
+          and people, driving accessibility, adoption, and meaningful impact.
+        </p>
+      </div>
 
-        {/* Canvas Component for ThreeJS render */}
-        <div  className="w-full h-full absolute inset-0">
-            <Canvas className="w-full h-full">
-                <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-                <HackerRoom/>
-            </Canvas>
-        </div>
+      {/* Canvas Component for ThreeJS render */}
+      <div className="w-full h-full absolute inset-0">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+              scale={sizes.deskScale}
+            />
+            <ambientLight intensity={1} />
+            <directionalLight intensity={0.5} position={[10, 10, 10]} />
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
